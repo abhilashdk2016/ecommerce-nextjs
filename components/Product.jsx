7009f36604dbbe2203d685328e29c25fb0667d0e
abhilashdk2016 <65328853+abhilashdkadv@users.noreply.github.com>
2023-04-01 11:40:19 +0530

    Product Details

---
+++ components/Product.jsx
@@ -0,0 +1,19 @@
+import { urlFor } from '@/lib/client';
+import Link from 'next/link';
+import React from 'react';
+
+const Product = ({ product: { image, name, slug, price } }) => {
+  return (
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className='product-card'>
+          <img src={urlFor(image && image[0])} width={250} height={250} className="product-image"/>
+        </div>
+        <p className='product-name'>{name}</p>
+        <p className='product-price'>${price}</p>
+      </Link>
+    </div>
+  )
+}
+
+export default Product
---
+++ sanity_ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'fev2v2qh',
+    dataset: 'production'
+  }
+})
